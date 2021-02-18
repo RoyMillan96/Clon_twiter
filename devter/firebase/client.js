1cbbd9d25b65e1238274d898e7375c0b50cd91c6
RoyMillan96 <rodrigo.millan@reinamadre.mx>
2021-02-18 16:51:25 -0600

    configuración de firestore para guardar y recuperar devits

--- devter/firebase/client.js
+++ devter/firebase/client.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 // Add the Firebase products that you want to use
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD74TKnCe__AObej7iBMpqKwjlV88ZUqY0",
@@ -15,12 +16,16 @@ const firebaseConfig = {
 // Initialize Firebase
 !firebase.apps.length && firebase.initializeApp(firebaseConfig);
 
+// inicializar el servicio de firestore para guardar info
+const db = firebase.firestore()
+
 const mapUserFromFirebaseAuthToUser = (user) => {
-  const { displayName, email, photoURL } = user;
+  const { displayName, email, photoURL, uid } = user;
   return {
     avatar: photoURL,
     username: displayName,
     email,
+    uid
   };
 };
 
@@ -35,3 +40,41 @@ export const loginWithGitHub = () => {
   const githubProvider = new firebase.auth.GithubAuthProvider();
   return firebase.auth().signInWithPopup(githubProvider);
 };
+
+// guardar info en BD de firestore 
+export const addDevit = ({ avatar, content, userId, userName }) => {
+  return db.collection("devits").add({
+    avatar,
+    content,
+    userId,
+    userName,
+    createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+    likesCount: 0,
+    sharedCount: 0,
+  })
+}
+
+// Lista de devits en la bd de firebase
+export const fetchLatestDevits = () => {
+  return db
+    .collection("devits")
+    .get()
+    .then(({ docs }) => {
+      return docs.map((doc) => {
+        const data = doc.data()
+        const id = doc.id
+        const { createdAt } = data
+
+        const date = new Date(createdAt.seconds * 1000)
+        const normalizedCreatedAt = new Intl.DateTimeFormat("es-ES").format(
+          date
+        )
+
+        return {
+          ...data,
+          id,
+          createdAt: normalizedCreatedAt,
+        }
+      })
+    })
+}
