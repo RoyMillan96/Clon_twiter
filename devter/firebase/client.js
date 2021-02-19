04e1f27c74ca42c2e48658769745b85cc1e0dee9
RoyMillan96 <rodrigo.millan@reinamadre.mx>
2021-02-18 22:19:33 -0600

    se configura el firestorage para guardar y recuperar la imagen

--- devter/firebase/client.js
+++ devter/firebase/client.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD74TKnCe__AObej7iBMpqKwjlV88ZUqY0",
@@ -42,10 +43,11 @@ export const loginWithGitHub = () => {
 };
 
 // guardar info en BD de firestore 
-export const addDevit = ({ avatar, content, userId, userName }) => {
+export const addDevit = ({ avatar, content, img, userId, userName }) => {
   return db.collection("devits").add({
     avatar,
     content,
+    img,
     userId,
     userName,
     createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
@@ -58,6 +60,7 @@ export const addDevit = ({ avatar, content, userId, userName }) => {
 export const fetchLatestDevits = () => {
   return db
     .collection("devits")
+    .orderBy("createdAt", "desc")
     .get()
     .then(({ docs }) => {
       return docs.map((doc) => {
@@ -65,16 +68,18 @@ export const fetchLatestDevits = () => {
         const id = doc.id
         const { createdAt } = data
 
-        const date = new Date(createdAt.seconds * 1000)
-        const normalizedCreatedAt = new Intl.DateTimeFormat("es-ES").format(
-          date
-        )
-
         return {
           ...data,
           id,
-          createdAt: normalizedCreatedAt,
+          createdAt: +createdAt.toDate(),
         }
       })
     })
+}
+
+// guardar una imagen en firestorage
+export const uploadImage = (file) => {
+  const ref = firebase.storage().ref(`images/${file.name}`)
+  const task = ref.put(file)
+  return task
 }
