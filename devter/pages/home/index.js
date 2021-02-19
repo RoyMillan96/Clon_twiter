214eaed91f247600a36a22e17110eff2967be49e
RoyMillan96 <rodrigo.millan@reinamadre.mx>
2021-02-18 22:21:34 -0600

    se agregan iconos y muestra la imagen en el home

--- devter/pages/home/index.js
+++ devter/pages/home/index.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react"
 import { fetchLatestDevits } from "firebase/client"
 import styles from "./styles.module.css"
+import Link from "next/link"
 import Layouts from "componentes/Layouts"
 import Devit from "componentes/Devit"
 import useUser from "hooks/useUser"
+import Create from "componentes/Icons/Create"
+import Home from "componentes/Icons/Home"
+import Search from "componentes/Icons/Search"
+import Head from "next/head"
 
 export default function HomePage() {
     const [timeline, setTimeline] = useState([])
@@ -16,15 +21,19 @@ export default function HomePage() {
     return (
         <>
             <Layouts>
+                <Head>
+                    <title>Inicio / Devter</title>
+                </Head>
                 <header className={styles.header}>
                     <h2 className={styles.h2}>Inicio</h2>
                 </header>
                 <section className={styles.section}>
-                    {timeline.map(({ id, userName, avatar, content, userId, createdAt }) => (
+                    {timeline.map(({ id, userName, avatar, content, userId, createdAt, img }) => (
                         <Devit
                             avatar={avatar}
                             createdAt={createdAt}
                             id={id}
+                            img={img}
                             key={id}
                             content={content}
                             userName={userName}
@@ -32,7 +41,23 @@ export default function HomePage() {
                         />
                     ))}
                 </section>
-                <nav className={styles.nav}></nav>
+                <nav className={styles.nav}>
+                    <Link href="/home">
+                        <a>
+                            <Home width={32} height={32} stroke="#09f" />
+                        </a>
+                    </Link>
+                    <Link href="/search">
+                        <a>
+                            <Search width={32} height={32} stroke="#09f" />
+                        </a>
+                    </Link>
+                    <Link href="/compose/tweet">
+                        <a>
+                            <Create width={32} height={32} stroke="#09f" />
+                        </a>
+                    </Link>
+                </nav>
             </Layouts>
         </>
     )
