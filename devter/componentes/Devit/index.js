0b059ef570c4dc928028aa5bd949fd234f84d607
RoyMillan96 <rodrigo.millan@reinamadre.mx>
2021-02-23 14:17:50 -0600

    correción del useTimeFormat

--- devter/componentes/Devit/index.js
+++ devter/componentes/Devit/index.js
@@ -1,17 +1,25 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 import styles from "./styles.module.css"
 import Avatar from "componentes/Avatar"
 import useTimeAgo from "hooks/useTimeAgo"
-// import useDateTimeFormat from "hooks/useDateTimeFormat"
+import useDateTimeFormat from "hooks/useDateTimeFormat"
 
 
 export default function Devit({ avatar, userName, content, id, createdAt, img }) {
     const timeago = useTimeAgo(createdAt)
-    // const createdAtFormated = useDateTimeFormat(createdAt)
+    const createdAtFormated = useDateTimeFormat(createdAt)
+    const router = useRouter()
+
+
+    const handleArticleClick = (e) => {
+        e.preventDefault()
+        router.push("/status/[id]", `/status/${id}`)
+    }
 
     return (
         <>
-            <article className={styles.article}>
+            <article className={styles.article} onClick={handleArticleClick}>
                 <div className={styles.div}>
                     <Avatar alt={userName} src={avatar} />
                 </div>
@@ -20,8 +28,8 @@ export default function Devit({ avatar, userName, content, id, createdAt, img })
                         <strong>{userName}</strong>
                         <span className={styles.span}>·</span>
                         <Link href={`/status/[id]`} as={`/status/${id}`}>
-                            <a>
-                                <time className={styles.time} >{timeago}</time>
+                            <a className={styles.a}>
+                                <time className={styles.time} title={createdAtFormated}>{timeago}</time>
                             </a>
                         </Link>
                     </header>
